a0f5c8667b8ff84f3729034c77c87d64fbc9b79c
Ernesto Tarrillo <etarrillo.pe@gmail.com>
2014-07-14 01:51:36 -0500

    Terminado el index principal con los nuevos cambios

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ $(function () {
 $(document).ready(function() {            
 	$().UItoTop({ easingType: 'easeOutQuart' });            
 });
+
+jQuery(function(){	
+	jQuery('#camera_wrap_1').camera({
+		thumbnails: false,
+		height: '350px',
+		fx: 'mosaic'
+	});
+});
