21f9ff2d84cf498abd9e04a661d30df85d316c44
Ernesto Tarrillo <etarrillo.pe@gmail.com>
2014-07-22 08:11:51 -0500

    Avances mientras viajaba

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ jQuery(function(){
 		fx: 'mosaic'
 	});
 });
+
+$(document).ready(function() {
+  var menu = $('#menu');
+  var contenedor = $('#menu-contenedor');
+  var menu_offset = menu.offset();
+  // Cada vez que se haga scroll en la página
+  // haremos un chequeo del estado del menú
+  // y lo vamos a alternar entre 'fixed' y 'static'.
+  $(window).on('scroll', function() {
+    if($(window).scrollTop() > menu_offset.top) {
+      menu.addClass('menu-fijo');
+    } else {
+      menu.removeClass('menu-fijo');
+    }
+  });
+});
